4d2e1da7f807ccee3adb86f66be81d3a19394f1f
duanmingqiang <964178976@qq.com>
2020-12-19 14:08:12 +0800

    修改travis中depoly配置

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env sh
 
 # 确保脚本抛出遇到的错误
-set -e
+#set -e
 
 # 生成静态文件
-npm run docs:build
+#npm run docs:build
 
 # 进入生成的文件夹
-cd docs/.vuepress/dist
+#cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
-git init
-git add -A
-git commit -m 'deploy'
+#git init
+#git add -A
+#git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
@@ -22,4 +22,4 @@ git commit -m 'deploy'
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 #  git push -f git@github.com:duanmingqiang/notes.git master:gh-pages
 
-cd -
+#cd -
